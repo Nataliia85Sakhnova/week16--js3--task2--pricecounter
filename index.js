6458ce23746b5616c0ede264856377a0fc797668
Razhatawka <nataliia85sakhnova@gmail.com>
2022-05-25 06:55:03 +0300

    еще чего то

--- index.js
+++ index.js
@@ -5,38 +5,37 @@
 document.getElementById('firms').onchange = function() {
     const value = document.getElementById('firms').value
             
-    const fuel [[0, 'бензин'], [-1000, `дизель`], [5000, `электро`]]
-    const volumes [[0, 1.0], [1000, 1.2], [2000, 1.8], [3000, 2.0]]
+    const fuel = [[0, 'бензин'], [-1000, `дизель`], [5000, `электро`]]
+    const fuelshort = [[0, 'бензин'], [-1000, `дизель`]]
+    const volumes = [[0, 1.0], [1000, 1.2], [2000, 1.8], [3000, 2.0]]
     
     let values=[] // создаем массив, в который будем записывать опции
     document.getElementById('container').innerHTML=''
+    //document.getElementById('container').innerHTML=values=[] - одно и то же?
       
     if(value==70000){
-      select.disabled=true //прописать следующий селектор (объем движка)
+      select.disabled=true
       return
     }
         else if(value==30000){
-          values = [[0, 'бензин'], [5000, `электро`]]
+          values = [...fuel]
         }
         else if(value==2000){
-          values = [[0, 'бензин'], [-1000, `дизель`]]
+          values = [...fuelshort]//а как вывести тот же массив но без некоторых опшнс?
+          // values = [...fuel[!2]]?
         }
-    
-    document.getElementById('shop').onchange = () =>{
-     const value = document.getElementById('lstFruits').value
-      if(value==0){
-        document.getElementById('container').innerHTML=''
-        return
       }
-      else{
-        const initialPrice = document.getElementById('type').value; //'3'
-        const upscaleWash = document.querySelectorAll('input[name=wash]:checked')[0].value //'20'
-        
-        const cut = document.getElementById('cut') //
-        const upscaleCut = cut.checked? cut.value: 0; //'50'
-        
-        document.getElementById('container').innerText= `Итого: ${Number(initialPrice) + Number(upscaleWash) + Number(upscaleCut)}р.`
-      }
-      
-    }
-    
+
+const select = document.createElement("select");
+select.name = volumes;
+select.id = volumes;
+
+for (const val of values)
+{
+  const option = document.createElement("option");
+  option.value = val;
+  select.appendChild(option);
+}
+
+
+ document.getElementById("container").appendChild(select);
