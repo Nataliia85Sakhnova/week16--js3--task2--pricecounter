a356399d0cdd9b598c0abfa64eb977c753b7285a
Razhatawka <nataliia85sakhnova@gmail.com>
2022-05-31 06:46:34 +0300

    еще не работает, но уже не сломано

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
 //  Выбор того или иного элемента должен влиять на результирующую цену
 // (*подсказка - суммы опций и вариантов комплектации можно хранить в value соответствующих
 // input-ов и option-ов, а потом просуммировать их в цикле*)
+const handlechange = () => {
+  const value = document.getElementById('firms').value
 
-document.getElementById('firms').onchange = function() {
-    const value = document.getElementById('firms').value
-            
-    const fuel = [[0, 'бензин'], [-1000, `дизель`], [5000, `электро`]]
-    const fuelshort = [[0, 'бензин'], [-1000, `дизель`]]
-    const volumes = [[0, 1.0], [1000, 1.2], [2000, 1.8], [3000, 2.0]]
-    
-    let values=[] // создаем массив, в который будем записывать опции
-    document.getElementById('container').innerHTML=''
-    //document.getElementById('container').innerHTML=values=[] - одно и то же?
-      
-    if(value==70000){
-      select.disabled=true
-      return
-    }
-        else if(value==30000){
-          values = [...fuel]
-        }
-        else if(value==2000){
-          values = [...fuelshort]//а как вывести тот же массив но без некоторых опшнс?
-          // values = [...fuel[!2]]?
-        }
-      }
+  const fuel = [
+    [0, 'бензин'],
+    [-1000, `дизель`],
+    [5000, `электро`]
+  ]
+  const volume = [
+    [0, 1.0],
+    [1000, 1.2],
+    [2000, 1.3],
+    [3000, 2.0]
+  ]
 
-const select = document.createElement("select");
-select.name = volumes;
-select.id = volumes;
+  let values = []
+  document.getElementById('container').innerHTML = ''
 
-for (const val of values)
-{
-  const option = document.createElement("option");
-  option.value = val;
-  select.appendChild(option);
+  if (value == 70000) {
+    values = fuel.filter((option) => option[1] === "электро")
+  } else if (value == 30000) {
+    values = [...fuel]
+  } else if (value == 2000) {
+    values = fuel.filter((option) => option[1] !== "электро")
+  }
+  console.log(values);
+
+  const select = document.createElement("select");
+  select.name = 'volumes';
+  select.id = 'volumes';
+
+  if (values.length === 1) {
+    select.disabled = true
+  }
+
+  for (const val of values) {
+    const option = document.createElement("option");
+    option.value = val[0];
+    option.text = val[1];
+    select.appendChild(option);
+
+  }
+
+  document.getElementById("container").appendChild(select);
 }
 
+const firmPrice = document.getElementById('firms').value;
+console.log(document.getElementById('firms').value);
+// const fuelPrice = ;
+// const typePrice = document.getElementById('type').value;
+const hotAssPrice = fireAss.checked ? fireAss.value : 0;
 
- document.getElementById("container").appendChild(select);
+
+// document.getElementById('container').innerText = `Цена: ${Number(firmPrice) + Number(fuelPrice) + Number(typePrice) + Number(fireAssPrice)}€.`
+
+document.getElementById('firms').onchange = handlechange;
+document.addEventListener("DOMContentLoaded", handlechange);
